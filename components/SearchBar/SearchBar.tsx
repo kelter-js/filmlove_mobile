80283d0facde010a6ae0d80e302ab985db81b62c
kelter-js <spawnkillerqw@gmail.com>
2025-11-20 15:29:36 +0500

    feat: SearchBar component

---
+++ components/SearchBar/SearchBar.tsx
@@ -0,0 +1,36 @@
+import { icons } from "@/constants/icons";
+import { FC } from "react";
+import { Image, TextInput, View } from "react-native";
+
+interface SearchBarProps {
+  placeholder: string;
+  onPress: VoidFunction;
+  value?: string;
+  onChangeText?: (value: string) => void;
+}
+
+export const SearchBar: FC<SearchBarProps> = ({
+  placeholder,
+  onPress,
+  value,
+  onChangeText,
+}) => {
+  return (
+    <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
+      <Image
+        source={icons.search}
+        className="size-5"
+        resizeMode="contain"
+        tintColor="#ab8bff"
+      />
+      <TextInput
+        onPress={onPress}
+        placeholder={placeholder}
+        value={value}
+        onChangeText={onChangeText}
+        placeholderTextColor="#a8b5db"
+        className="flex-1 ml-2 text-white"
+      />
+    </View>
+  );
+};
